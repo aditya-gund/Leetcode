4cd7c2cde98319610f0fe19edaf97e35e489ae42
Aditya <adityagundg14@gmail.com>
2023-03-10 18:13:52 +0530

    Warmup Questions

---
+++ Leetecode/src/arrays/and/maths/FizzBuzz.java
@@ -0,0 +1,29 @@
+package arrays.and.maths;
+import java.util.*;
+public class FizzBuzz {
+
+	public List<String> fizzBuzz(int n){
+		ArrayList<String> ans = new ArrayList<>();
+		for(int i=1;i<=n;i++) {
+			if(i%3==0 && i%5==0)
+				ans.add("FizzBuzz");
+			else if(i%3==0)
+				ans.add("Fizz");
+			else if(i%5==0)
+				ans.add("Buzz");
+			else
+				ans.add(i+"");
+		}
+		return ans;
+	}
+	public static void main(String[] args) {
+			FizzBuzz fz = new FizzBuzz();
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter a number: ");
+			int n=sc.nextInt();
+			System.out.println(fz.fizzBuzz(15));
+			
+		
+	}
+
+}
